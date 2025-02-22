2501b0b731328c16d33ec345e9f4c33335033242
Yeli.James <jbmalvar.cs.washington.edu>
2025-02-22 13:38:06 -0800

    Added Images and made each one unique

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import './App.css';
 import marvelRivalsImage from './assets/MarvelRivalsImage.jpg'
 import jeff from './assets/jefftheshark.webp'
 import Card from './components/card.jsx'
+import diveChar from './assets/diveChar.avif'
+import BeatComp from './assets/BeatComp.jpg'
+import top500 from './assets/OneAboveAll.webp'
+import tierlist from './assets/tierlist.png'
+import psylocke from './assets/psylocke.webp'
+import metaComp from './assets/MarvelRivalsComp.webp'
+import ranks from './assets/ranks.png'
+import tips from  './assets/tips.avif'
+import counters from './assets/counters.jpg'
 
 const App = () => {
 
@@ -14,54 +23,54 @@ const App = () => {
       <h2>Marvel Rivals stuff to look at</h2>
       <div className = "card-container">
         <Card
-          image={jeff}
+          image={tierlist}
           title="Tier List"
+          description="Tier List for all heroes"
+        />
+        <Card
+          image={counters}
+          title="Counters"
           description="A description of the card"
         />
         <Card
-          image={jeff}
-          title="Tier List"
-          description="A description of the card"
+          image={psylocke}
+          title="Recommended Heroes"
+          description= "Recommended Heroes for first time players"
+        />
+        <Card
+          image={top500}
+          title="Top 500"
+          description="Current top 500 players"
+        />
+        <Card
+          image={ranks}
+          title="Rank Guide"
+          description="How to climb in certain ranks"
+        />
+        <Card
+          image={tips}
+          title="Tips & Tricks"
+          description="Tips and tricks for the game"
+        />
+        <Card
+          image={metaComp}
+          title="Meta Comps"
+          description="Current Meta Comps"
         />
         <Card
           image={jeff}
-          title="Tier List"
-          description="A description of the card"
+          title="Jeff Images"
+          description="Images of Jeff"
         />
         <Card
-          image={jeff}
-          title="Tier List"
-          description="A description of the card"
+          image={BeatComp}
+          title="How to beat comp"
+          description="Quick guide to beat comps"
         />
         <Card
-          image={jeff}
-          title="Tier List"
-          description="A description of the card"
-        />
-        <Card
-          image={jeff}
-          title="Tier List"
-          description="A description of the card"
-        />
-        <Card
-          image={jeff}
-          title="Tier List"
-          description="A description of the card"
-        />
-        <Card
-          image={jeff}
-          title="Tier List"
-          description="A description of the card"
-        />
-        <Card
-          image={jeff}
-          title="Tier List"
-          description="A description of the card"
-        />
-        <Card
-          image={jeff}
-          title="Tier List"
-          description="A description of the card"
+          image={diveChar}
+          title="Counter Dives"
+          description="Quick guide to counter dives"
         />
       </div>
     </div>
